4d7691d213f26adb02d591371ce396c1d4015fcf
Richard Liu Wang <17093797+rclw@users.noreply.github.com>
2026-01-16 03:23:33 +0000

    Trying harder

--- client/src/lib/treeparse.ts
+++ client/src/lib/treeparse.ts
@@ -56,16 +56,15 @@ export function convertToDerivation(nodes: Node[], edges: Edge[]): string {
     const numChildren = Math.max((childMap[nodeId] || []).length, 1);
     const vlin = `vl${'i'.repeat(numChildren)}n`;
 
+    // Print node
+    lines.push(`${indentStr}\\${vlin}{${notes[0]}}{${notes[1]}}{${expr}}`);
+    
     // Print children
     const children = childMap[nodeId] || [];
     if (children.length === 0) {
-      lines.push(`${indentStr}{`);
-      lines.push(`${indentStr}  \\vlhy{}`);
-      lines.push(`${indentStr}}`);
+      lines.push(`${indentStr}\\vlhy{${expr}`);
     } else {
       for (const child of children) {
-        // Print node
-        lines.push(`${indentStr}\\${vlin}{${notes[0]}}{${notes[1]}}{${expr}}`);
         lines.push(`${indentStr}{`);
         printTree(child, indent + 1);
         lines.push(`${indentStr}}`);
